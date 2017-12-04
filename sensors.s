075b36d7511cd2072d28f184e01d5bedfca0520b
Noah NU <noahnu@gmail.com>
2017-12-04 15:30:43 -0500

    IS_OBSTACLE_AHEAD now checks for out of bound errors

--- sensors.s
+++ sensors.s
@@ -7,10 +7,27 @@
  *
  * Determines if there is an obstacle ahead. Returns 1 if there's
  * an obstacle, 0 otherwise.
+ *
+ * A map edge is also considered an "obstacle", although we don't record it
+ * in the grid array. For map edges, we can just check XY coordinates and
+ * compare with what we know of the map's boundaries.
  */
 IS_OBSTACLE_AHEAD:
-    addi sp, sp, -4
-    stw r16, 0(sp)
+    addi sp, sp, -16
+    stw ra, 0(sp)
+    stw r16, 4(sp)
+    stw r4, 8(sp)
+    stw r5, 12(sp)
+
+    /* Check if moving one forward would result in out-of-bounds. */
+    movia r2, CURRENT_POSITION
+    movia r3, CURRENT_DIRECTION
+    ldw r4, 0(r2)
+    ldw r5, 0(r3)
+    call GET_NEXT_CELL
+
+    /* If out of bound error, 'obstacle' exists in forward direction. */
+    beq r2, r0, OBSTACLE_EXISTS
 
     /* Enable Sensor 0. */
 	movia r2, LEGO_CONTROLLER
@@ -41,7 +58,10 @@ OBSTACLE_EXISTS:
     movi r2, 1
 
 DONE:
-    ldw r16, 0(sp)
-    addi sp, sp, 4
+    ldw ra, 0(sp)
+    ldw r16, 4(sp)
+    ldw r4, 8(sp)
+    ldw r5, 12(sp)
+    addi sp, sp, 16
 
     ret
